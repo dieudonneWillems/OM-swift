d146263f5a20f0f3dfd02c41d041e60d05e37a2b
lapsedPacifist <don_willems@me.com>
2015-07-06 18:15:11 +0200

    Started on OM project
    Created project in XCode.
    Added gitignore

---
+++ OM/OM.h
@@ -0,0 +1,19 @@
+//
+//  OM.h
+//  OM
+//
+//  Created by Don Willems on 06/07/15.
+//  Copyright (c) 2015 lapsedpacifist. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for OM.
+FOUNDATION_EXPORT double OMVersionNumber;
+
+//! Project version string for OM.
+FOUNDATION_EXPORT const unsigned char OMVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OM/PublicHeader.h>
+
+
